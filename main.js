d035514e18de4169001503932ab9ea5086ab5b07
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2025-03-29 08:51:00 +0630

    unit testing

---
+++ main.js
@@ -0,0 +1,53 @@
+// Test Suite
+
+describe(`${User.name} Class`, () => {
+  let model;
+
+  beforeEach(() => {
+    model = new User();
+  });
+
+  describe("default values", () => {
+    it("first name defaults to empty", () => {
+      // assert
+      expect(model.firstName).toBe("");
+    });
+
+    it("last name defaults to empty", () => {
+      // assert
+      expect(model.lastName).toBe("");
+    });
+
+    it("middle name defaults to empty", () => {
+      // assert
+      expect(model.middleName).toBe("");
+    });
+  });
+  describe("full name", () => {
+    beforeEach(() => {
+      model = new User({ firstName: "Thet", lastName: "Soe" });
+    });
+    it("middle initial when middleName is defined with first and last", () => {
+      // arrange
+      model.middleName = "Naing";
+
+      // act
+      const result = model.fullName;
+
+      // assert
+      expect(result).toBe(
+        `${model.firstName} ${model.middleName[0]}. ${model.lastName}`
+      );
+    });
+    it("when no middle name return just first and last", () => {
+      // arrange
+      model.middleName = "";
+
+      // act
+      const result = model.fullName;
+
+      // assert
+      expect(result).toBe(`${model.firstName} ${model.lastName}`);
+    });
+  });
+});
